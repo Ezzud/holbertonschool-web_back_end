1dbc95c2e760684076df2c85bd9ba2f36cc3931d
Ezzud <contact@ezzud.fr>
2024-07-24 10:40:00 +0200

    Update brackets on loops

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const final = [];
-  for (const value of array)
+  for (const value of array) {
     final.push(appendString + value);
+  }
 
   return final;
 }
