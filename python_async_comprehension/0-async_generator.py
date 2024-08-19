049dc35f851bf5f22006a9c67f6aa001a3f4adb0
Ezzud <contact@ezzud.fr>
2024-08-19 10:15:00 +0200

    Project 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Write a coroutine called async_generator
+with no argument"""
+
+import asyncio
+import random
+from typing import List, Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """The coroutine will loop 10 times, wait 
+    10 seconds, and give a random number"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""coroutine that execute asynchronously"""
+
+import time
+from typing import List, Generator
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure the total runtime and return it"""
+    start = time.time()
+    await asyncio.gather(async_comprehension())
+    end = time.time()
+    total = end - start
+    return total
