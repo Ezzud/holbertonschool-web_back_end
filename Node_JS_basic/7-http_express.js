d33effebab937627dc096989598500b1770737e7
Ezzud <contact@ezzud.fr>
2024-09-20 10:12:50 +0200

    Update

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -40,7 +40,7 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+    res.send("Hello Holberton School!");
 });
 app.get('/students', (req, res) => {
     res.write('This is the list of our students\n');
