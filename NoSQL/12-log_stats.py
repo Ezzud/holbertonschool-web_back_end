6754e8640ee17da36678060a1a4858ce993a286b
Ezzud <contact@ezzud.fr>
2024-12-15 21:07:34 +0100

    Update log stats

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,6 +3,7 @@
 Python script that provides some stats about Nginx logs stored in MongoDB.
 """
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 
 def log_infos() -> None:
@@ -10,29 +11,22 @@ def log_infos() -> None:
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    try:
+        client = MongoClient('mongodb://127.0.0.1:27017')
+        client.admin.command('ping')  # Check if the server is available
+    except ConnectionFailure:
+        print("Server not available")
+        return
+
     nginx = client.logs.nginx
 
-    print(
-        "{} logs".format(
-            nginx.count_documents({})
-        )
-    )
+    print(f"{nginx.count_documents({})} logs")
 
     print("Methods:")
-
     for method in methods:
-        print(
-            "\tmethod {}: {}".format(
-                method, nginx.count_documents({'method': method})
-            )
-        )
+        print(f"\tmethod {method}: {nginx.count_documents({'method': method})}")
 
-    print(
-        "{} status check".format(
-            nginx.count_documents({'method': 'GET', 'path': '/status'})
-        )
-    )
+    print(f"{nginx.count_documents({'method': 'GET', 'path': '/status'})} status check")
 
 
 if __name__ == "__main__":
