19f4e059be032c34ad53ec58493a545f7011102f
Ezzud <contact@ezzud.fr>
2025-01-24 09:56:52 +0100

    Eslint fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
   if ((name) !== null) {
-	process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 process.stdin.on('end', () => {
